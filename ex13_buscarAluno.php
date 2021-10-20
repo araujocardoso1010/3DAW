406e5f3949151328cfbcd5fb557402478bc885d1
araujocardoso1010 <80277365+araujocardoso1010@users.noreply.github.com>
2021-10-20 10:56:38 -0300

    Update ex13_buscarAluno.php

--- ex13_buscarAluno.php
+++ ex13_buscarAluno.php
@@ -5,11 +5,11 @@
 		<title>Informe a matrícula</title>
 	</head>
 	<body>
-		<a href="a13_cadastroAlunoArquivo.php">Novo Aluno</a><br>
-		<a href="a13_buscarAluno.php">Alterar Aluno</a><br>
-		<a href="a13_listarAlunos.php">Listar Alunos</a>
+		<a href="ex13_cadastroAlunoArquivo.php">Novo Aluno</a><br>
+		<a href="ex13_buscarAluno.php">Alterar Aluno</a><br>
+		<a href="ex13_listarAlunos.php">Listar Alunos</a>
 		<h1>Buscar Aluno</h1>
-		<form method="POST" action="a13_atualizarAluno.php">
+		<form method="POST" action="ex13_atualizarAluno.php">
 			Informe a matrícula do aluno:<br>
 			<input type="text" name="matricula">
 			<input type="submit" name="buscar" value="Buscar">
