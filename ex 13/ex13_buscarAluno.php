1804995118fe1a84865223706a0aa1458b9121d1
araujocardoso1010 <80277365+araujocardoso1010@users.noreply.github.com>
2021-10-28 10:33:04 -0300

    Update ex13_buscarAluno.php

--- ex 13/ex13_buscarAluno.php
+++ ex 13/ex13_buscarAluno.php
@@ -1,3 +1,10 @@
+<?php
+	if($_GET['del']):
+		$action_link = 'ex13_excluirAluno.php';
+	else:
+		$action_link = 'ex13_atualizarAluno.php';
+	endif;
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
@@ -6,10 +13,11 @@
 	</head>
 	<body>
 		<a href="ex13_cadastroAlunoArquivo.php">Novo Aluno</a><br>
-		<a href="ex13_buscarAluno.php">Alterar Aluno</a><br>
+		<a href="ex13_buscarAluno.php?del=">Alterar Aluno</a><br>
+		<a href="ex13_buscarAluno.php?del=1">Excluir Aluno</a><br>
 		<a href="ex13_listarAlunos.php">Listar Alunos</a>
 		<h1>Buscar Aluno</h1>
-		<form method="POST" action="ex13_atualizarAluno.php">
+		<form method="POST" action="<?php echo $action_link;?>">
 			Informe a matrícula do aluno:<br>
 			<input type="text" name="matricula">
 			<input type="submit" name="buscar" value="Buscar">
