b006e7c4a822efc32934ae1d9d2110230abe07cb
araujocardoso1010 <80277365+araujocardoso1010@users.noreply.github.com>
2021-11-04 11:33:04 -0300

    Create ex14_buscarAluno.php

---
+++ ex 14/ex14_buscarAluno.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Informe a matrícula</title>
+	</head>
+	<body>
+		<h1>Buscar Aluno</h1>
+		<form method="GET" action="ex14_alterarAluno.php">
+			Informe a matrícula do aluno:<br>
+			<input type="text" name="matricula">
+			<input type="submit" name="buscar" value="Buscar">
+		</form>
+	</body>
+</html>
